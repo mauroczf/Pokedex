e9640ffaae96b0ba10e2fb1a0fcc582b13c9f5a3
mauroczf <mauroczf@gmail.com>
2023-02-10 19:50:33 -0400

    fix: vue dependencies for project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   vuetify,
-  el: '#app'
-})
+  render: h => h(App)
+}).$mount('#app')
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.s(c|a)ss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              // Requires sass-loader@^7.0.0
+              options: {
+                implementation: require('sass'),
+                indentedSyntax: true // optional
+              },
+            },
+          ],
+        },
+      ],
+    }
+  }
