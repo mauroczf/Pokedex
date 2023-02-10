f7138dce8586c7c7c183931befe79fc8554e2356
mauroczf <mauroczf@gmail.com>
2023-02-09 21:01:53 -0400

    vuetify install

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  vuetify,
+  el: '#app'
 })
