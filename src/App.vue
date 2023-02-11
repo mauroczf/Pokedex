de79640b63da7c9b307b9fa31557d97106b26f9e
mauroczf <mauroczf@gmail.com>
2023-02-10 21:59:46 -0400

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,114 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-container>
+      <v-container>
+        <v-row>
+          <v-container>
+            <v-img
+              :src="require('../src/assets/pokedex.png')"
+              class="my-3"
+              contain
+              height="200"
+            />
+            <h1 class="text-center white--text mb-8" style="font-size: 5rem">
+             Pokedex
+            </h1>
+          </v-container>
+        </v-row>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Charmander"
+          solo
+        ></v-text-field>
 
-      <v-spacer></v-spacer>
+        <v-row>
+          <v-col
+            cols="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <PokemonInfoDialog
+      :show.sync="show_dialog"
+      :selected_pokemon="selected_pokemon"
+    />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import axios from "axios";
+import PokemonCard from "./components/PokemonCard.vue";
+import PokemonInfoDialog from "./components/PokemonInfoDialog.vue";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    PokemonCard,
+    PokemonInfoDialog,
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      pokemons: [],
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods: {
+    show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+    },
+    get_move_level(move) {
+      for (let version of move.version_group_details) {
+        if (
+          version.version_group.name == "sword-shield" &&
+          version.move_learn_method.name == "level-up"
+        ) {
+          return version.level_learned_at;
+        }
+      }
+      return 0;
+    },
+  },
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      });
+    },
+  },
 };
 </script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
