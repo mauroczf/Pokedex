f31e3bb618b751f14f49ff50c2e04a6641587bce
mauroczf <mauroczf@gmail.com>
2023-02-10 21:54:06 -0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
-// webpack.config.js
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.s(c|a)ss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              // Requires sass-loader@^7.0.0
-              options: {
-                implementation: require('sass'),
-                indentedSyntax: true // optional
-              },
-            },
-          ],
-        },
-      ],
-    }
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
   }
+  return config;
+};
